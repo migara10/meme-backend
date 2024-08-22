0c553d0bb188a2286aa2f2fe598e0c45599fd004
migara tennakoon <migaraten@gmail.com>
2024-08-23 00:33:16 +0300

    add server v5

--- database/connection.js
+++ database/connection.js
@@ -1,19 +1,22 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+// Load environment variables from .env file
 dotenv.config();
 
-
 async function connect() {
-    try {
-      const db = await mongoose.connect(process.env.DATABASE_URL, {
-      });
-      console.log("Connected to MongoDB");
-      return db;
-    } catch (error) {
-      console.error("Error connecting to MongoDB:", error);
-    }
+  try {
+    // Connect to MongoDB using the DATABASE_URL from environment variables
+    const db = await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,   // Use the new MongoDB connection string parser
+      useUnifiedTopology: true, // Use the new unified topology engine for MongoDB
+    });
+    console.log("Connected to MongoDB");
+    return db;
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message || error);
+    process.exit(1); // Exit the process with failure
   }
-  
-  export default connect;
+}
 
+export default connect;
