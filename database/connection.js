288f4c7c3826426a9d18105e8c5399d53ded1422
migara tennakoon <migaraten@gmail.com>
2024-08-23 10:20:31 +0300

    add server v9

--- database/connection.js
+++ database/connection.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 async function connect() {
     try {
-      const db = await mongoose.connect('mongodb+srv://sphiriad:wo5eIHAnZvgRTBes@cluster0.qkuh3tq.mongodb.net/meme', {
+      const db = await mongoose.connect(process.env.DATABASE_URL, {
       });
       console.log("Connected to MongoDB");
       return db;
